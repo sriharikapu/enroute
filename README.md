f748bbd312e1c8d7ecea3c7e8f990b20659d7c79
zhenting zhou <zhentzhou@gmail.com>
2018-04-27 23:32:12 -0400

    test red color map

---
+++ README.md
@@ -0,0 +1,9 @@
+# enroute
+frontend demo for track and trace app for food
+
+users:
+
+  1. manufacturer
+  2. delivary truck
+  3. supermarket
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
+/*global google*/
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const { compose, withProps, withStateHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+} = require("react-google-maps");
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+const demoFancyMapStyles = require("./demoFancyMapStyles.json");
+
+const StyledMapWithAnInfoBox = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `1600px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+    center: { lat: 25.03, lng: 121.6 },
+  }),
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={9}
+    defaultCenter={props.center}
+    defaultOptions={{ styles: demoFancyMapStyles }}
+  >
+    <InfoBox
+      defaultPosition={new google.maps.LatLng(props.center.lat, props.center.lng)}
+      options={{ closeBoxURL: ``, enableEventPropagation: true }}
+    >
+      <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
+        <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+          Hello, Taipei!
+        </div>
+      </div>
+    </InfoBox>
+
+  </GoogleMap>
+);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <StyledMapWithAnInfoBox />
+        {/* what if position fixed */}
       </div>
     );
   }
